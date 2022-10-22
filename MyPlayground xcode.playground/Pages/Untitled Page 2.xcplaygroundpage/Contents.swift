850705fae3702955b00f836bd747bbfe736c5550
Palczyk69 <lukasz.holowacz5@gmail.com>
2022-10-22 09:07:22 +0200

    Initial Commit

---
+++ MyPlayground xcode.playground/Pages/Untitled Page 2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+//: [Previous](@previous)
+// Komentarz
+
+//: Dokumentacja
+//: # Dokmentacja
+//: ## Dokumentacja
+//: ### Dokumentacja
+
+
+import Foundation
+
+var greeting = "Hello, playground"
+
+//: [Next](@next)
